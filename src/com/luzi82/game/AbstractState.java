79b4bf232d90aec868729467387d66c6e7b4f95f
luzi82 <luzi82@gmail.com>
2011-04-03 12:03:53 +0800

    state add default empty func

--- src/com/luzi82/game/AbstractState.java
+++ src/com/luzi82/game/AbstractState.java
@@ -4,36 +4,47 @@ import android.graphics.Canvas;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 
-public abstract class AbstractState<P extends StateParent> implements StateParent {
+public abstract class AbstractState<P extends StateParent> implements
+		StateParent {
 
 	protected final P mParent;
 
 	protected AbstractState(P parent) {
 		mParent = parent;
 	}
-	
-	public P getParent(){
+
+	public P getParent() {
 		return mParent;
 	}
 
-	public abstract void onTouchEvent(MotionEvent event);
+	public void onTouchEvent(MotionEvent event) {
+	}
 
-	public abstract void onKeyDown(int keyCode, KeyEvent msg);
+	public void onKeyDown(int keyCode, KeyEvent msg) {
+	}
 
-	public abstract void onKeyUp(int keyCode, KeyEvent msg);
+	public void onKeyUp(int keyCode, KeyEvent msg) {
+	}
 
-	public abstract void onGameResume();
+	public void onGameResume() {
+	}
 
-	public abstract void onGamePause();
+	public void onGamePause() {
+	}
 
-	public abstract void onStateStart();
+	public void onStateStart() {
+	}
 
-	public abstract void onStateEnd();
+	public void onStateEnd() {
+	}
 
-	public abstract void surfaceChanged(int format, int width, int height);
+	public void surfaceChanged(int format, int width, int height) {
+	}
 
-	public abstract void draw(Canvas c);
+	public void draw(Canvas c) {
+	}
 
-	public abstract void tick();
+	public void tick() {
+	}
 
 }
